0fecf90d6a5a415029e432e05d1a18d698594429
IvanCardi <ivancardillo1996@gmail.com>
2025-03-03 17:13:27 +0100

    fix: adds string sanitation for car creation

--- src/modules/car/useCases/createCar/createCarController.ts
+++ src/modules/car/useCases/createCar/createCarController.ts
@@ -1,6 +1,6 @@
 import * as express from "express";
-import { BaseController, Error } from "../../../../shared";
-import { CreateCar } from "./createCar";
+import { BaseController, Error, TextUtils } from "../../../../shared";
+import { CreateCar, CreateCarInput } from "./createCar";
 
 export class CreateCarController extends BaseController {
   private createCar: CreateCar;
@@ -15,7 +15,19 @@ export class CreateCarController extends BaseController {
     res: express.Response
   ): Promise<any> {
     try {
-      await this.createCar.execute(req.body);
+      const input: CreateCarInput = {
+        brand: !!req.body.brand ? TextUtils.sanitize(req.body.brand) : "",
+        model: !!req.body.model ? TextUtils.sanitize(req.body.model) : "",
+        setup: !!req.body.setup ? TextUtils.sanitize(req.body.setup) : "",
+        description: !!req.body.description
+          ? TextUtils.sanitize(req.body.description)
+          : "",
+        plate: !!req.body.plate ? TextUtils.sanitize(req.body.plate) : "",
+        kilometers: req.body.kilometers,
+        year: req.body.year,
+      };
+
+      await this.createCar.execute(input);
 
       return this.created(res);
     } catch (error) {
