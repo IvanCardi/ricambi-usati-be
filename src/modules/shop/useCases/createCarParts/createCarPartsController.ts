63eb6a55c479995ec7ca8717fb33a86238d03222
IvanCardi <ivancardillo1996@gmail.com>
2025-03-04 12:45:35 +0100

    feat: adds sanitazion on create car parts

--- src/modules/shop/useCases/createCarParts/createCarPartsController.ts
+++ src/modules/shop/useCases/createCarParts/createCarPartsController.ts
@@ -1,6 +1,6 @@
 import * as express from "express";
-import { BaseController, Error } from "../../../../shared";
-import { CreateCarParts } from "./createCarParts";
+import { BaseController, Error, TextUtils } from "../../../../shared";
+import { CreateCarParts, CreateCarPartsInput } from "./createCarParts";
 
 export class CreateCarPartsController extends BaseController {
   private createCarParts: CreateCarParts;
@@ -15,9 +15,29 @@ export class CreateCarPartsController extends BaseController {
     res: express.Response
   ): Promise<object> {
     try {
-      await this.createCarParts.execute(req.body);
+      const input: CreateCarPartsInput = {
+        carId: req.body.carId,
+        parts: req.body.parts.map((p: any) => ({
+          name: !!p.name ? TextUtils.sanitize(p.name) : "",
+          numbers: !!p.numbers
+            ? p.numbers.map((n: string) => TextUtils.sanitize(n))
+            : [],
+          category: !!p.category ? TextUtils.sanitize(p.category) : "",
+          description: !!p.description ? TextUtils.sanitize(p.description) : "",
+          photos: !!p.photos
+            ? p.photos.map((p: string) => TextUtils.sanitize(p))
+            : [],
+          compatibleCars: !!p.compatibleCars
+            ? p.compatibleCars.map((c: string) => TextUtils.sanitize(c))
+            : [],
+          warranty: p.warranty, // in month
+          price: p.price, // in €
+        })),
+      };
 
-      return this.ok(res);
+      await this.createCarParts.execute(input);
+
+      return this.created(res);
     } catch (error) {
       if (error instanceof Error) {
         return this.clientError(res, error.message);
