5d327c76c72dbb10e6280ca1357faa38faf5b63b
IvanCardi <ivancardillo1996@gmail.com>
2025-05-12 21:58:55 +0200

    ref: adds authentication middleware to get user id from jwt

--- src/modules/shop/routes/orderDraftRouter.ts
+++ src/modules/shop/routes/orderDraftRouter.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { CreateOrUpdateOrderDraftController } from "../useCases/createOrUpdateOrderDraft/createOrUpdateOrderDraftController";
 import { createOrUpdateOrderDraft } from "../useCases/createOrUpdateOrderDraft";
+import { authentication } from "../../../middelwares/authentication";
 
 const orderDraftRouter = express.Router();
 
-orderDraftRouter.post("/orderDrafts", (req, res) =>
+orderDraftRouter.post("/orderDrafts", authentication, (req, res) =>
   new CreateOrUpdateOrderDraftController(createOrUpdateOrderDraft).execute(
     req,
     res
